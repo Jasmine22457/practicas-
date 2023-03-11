5ca643d2b6ca8612fa4c0f02c8cc78addd74a8a1
Jas Menjivar <mm0588032021@unab.edu.sv>
2023-03-11 11:16:04 -0600

    practicaZapatos

---
+++ PracticaZapatos/PracticaZapatos/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaZapatos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Zapatos zapato = new Zapatos();
+
+            int opc;
+
+
+            while (true)
+            {
+                Console.WriteLine("----------***Bienvenido a la zapateria en linea***--------------\n");
+                Console.WriteLine("Selecione una tienda \n");
+                Console.WriteLine("Menu");
+                Console.WriteLine("1-Zapateria Md");
+                Console.WriteLine("2-Zapateria Jose");
+                Console.WriteLine("3-Zapateria Andrea ");
+                Console.WriteLine("4-Salir");
+                opc = Convert.ToInt32(Console.ReadLine());
+
+                switch (opc)
+                {
+                    case 1:
+                        Console.WriteLine("-------**Zapateria Md**-------");
+                        Menu menuMd = new Menu();
+
+                        menuMd.Estilo();
+                        zapato.estilo = Console.ReadLine();
+
+                        menuMd.Marca();
+                        zapato.marca = Console.ReadLine();
+
+                        menuMd.Talla();
+                        zapato.talla = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("precio:");
+                        double p1 = Convert.ToDouble(Console.ReadLine());
+                        double desc = 0.05;
+
+                        zapato.precio = p1 - (p1 * desc);
+
+                        Console.WriteLine("\n-------**recibo**-------");
+                        Console.WriteLine("Estilo: " + zapato.Estilo);
+                        Console.WriteLine("Marca: " + zapato.Marca);
+                        Console.WriteLine("Sise: " + zapato.talla);
+                        Console.WriteLine("Descuento: " + desc);
+                        Console.WriteLine("Precio: $" + zapato.precio);
+                        break;
+                    case 2:
+                        Console.WriteLine("-------**Zapateria Jose**-------");
+                        Menu menuj = new Menu();
+
+                        menuj.Estilo();
+                        zapato.estilo = Console.ReadLine();
+
+                        menuj.Marca();
+                        zapato.marca = Console.ReadLine();
+
+                        menuj.Talla();
+                        zapato.talla = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("precio:");
+                        double p2 = Convert.ToDouble(Console.ReadLine());
+                        double desc2 = 0.10;
+
+                        zapato.precio = p2 - (p2 * desc2);
+
+                        Console.WriteLine("\n-------**Recibo**-------");
+                        Console.WriteLine("Estilo: " + zapato.Estilo);
+                        Console.WriteLine("Marca: " + zapato.Marca);
+                        Console.WriteLine("Sise: " + zapato.talla);
+                        Console.WriteLine("Descuento: " + desc2);
+                        Console.WriteLine("Precio: $" + zapato.precio);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("-------**Zapateria Andrea**-------");
+                        Menu menuA = new Menu();
+
+                        menuA.Estilo();
+                        zapato.estilo = Console.ReadLine();
+
+                        menuA.Marca();
+                        zapato.marca = Console.ReadLine();
+
+                        menuA.Talla();
+                        zapato.talla = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Digite el precio:");
+                        double p3 = Convert.ToDouble(Console.ReadLine());
+                        double desc3 = 0.15;
+
+                        zapato.precio = p3 - (p3 * desc3);
+
+                        Console.WriteLine("\n-------factura-------");
+                        Console.WriteLine("Estilo: " + zapato.Estilo);
+                        Console.WriteLine("Marca: " + zapato.Marca);
+                        Console.WriteLine("Sise: " + zapato.talla);
+                        Console.WriteLine("Descuento: " + desc3);
+                        Console.WriteLine("Precio: $" + zapato.precio);
+                        break;
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("La opcion no esta definida");
+                        break;
+                }
+            }
+
+
+
+
+        }
+
+    }
+}
+
+class Menu
+{
+    public void Estilo()
+    {
+        Console.WriteLine("Estilos");
+        Console.WriteLine("1-Sport");
+        Console.WriteLine("2-Casual");
+        Console.WriteLine("3-Botas");
+    }
+    public void Marca()
+    {
+        Console.WriteLine("Marcas");
+        Console.WriteLine("1-Nike");
+        Console.WriteLine("2-Converse");
+        Console.WriteLine("3-DrMartin");
+    }
+    public void Talla()
+    {
+        Console.WriteLine("Size");
+        Console.WriteLine("1-35");
+        Console.WriteLine("2-37");
+        Console.WriteLine("3-39");
+    }
+}
---
+++ PracticaZapatos/PracticaZapatos/Zapatos.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaZapatos
+{
+    internal class Zapatos
+    {
+
+        public string estilo;
+        public string marca;
+        public double talla;
+        public double precio;
+
+        public string Estilo
+        {
+            set { estilo = value; }
+            get
+            {
+
+                switch (estilo)
+                {
+                    case "1":
+                        this.estilo = "Sport";
+                        break;
+
+                    case "2":
+                        this.estilo = "Casual";
+                        break;
+
+                    case "3":
+                        this.estilo = "Botas";
+                        break;
+
+                    default:
+                        this.estilo = "Estilo de Zapato no seleccionado";
+                        break;
+                    case null:
+                        this.estilo += "No ha ingresado ningun estilo de  zapatos";
+                        break;
+
+                }
+                return this.estilo;
+            }
+        }
+
+        public string Marca
+        {
+            set { marca = value; }
+            get
+            {
+
+                switch (marca)
+                {
+                    case "1":
+                        this.marca = "Nike";
+                        break;
+
+                    case "2":
+                        this.marca = "Converse";
+                        break;
+
+                    case "3":
+                        this.marca = "DrMartin";
+                        break;
+
+                    default:
+                        this.marca = "La marca de Zapato no se a seleccionado";
+                        break;
+                    case null:
+                        this.marca += "No ha ingresado ninguna marca de zapato";
+                        break;
+
+                }
+                return this.marca;
+            }
+        }
+
+        public double Talla
+        {
+            set { talla = value; }
+            get
+            {
+
+                switch (talla)
+                {
+                    case 1:
+                        this.talla = 36;
+                        break;
+
+                    case 2:
+                        this.talla = 37;
+                        break;
+
+                    case 3:
+                        this.talla = 38;
+                        break;
+
+                    default:
+                        this.talla = 0;
+                        break;
+                }
+                return this.talla;
+            }
+        }
+    }
+}
